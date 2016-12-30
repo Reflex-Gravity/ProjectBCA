1f064c57adc3217072daff9985f6ab5478620f6b
Clinton Dsouza <clintondsouza96@gmail.com>
2016-12-30 22:43:13 +0530

    Init Commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>E-Wagon</title>
+      <!--All imports  -->
+      <link rel="shortcut icon" href="images/favicon/favicon.ico" type="image/x-icon">
+      <link rel="stylesheet" href="css/material.css">
+      <script src="js/material.min.js"></script>
+      <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+   </head>
+   <body>
+      <!--Left Alligned Drawer -->
+      <div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer">
+         <div class="mdl-layout__drawer">
+            <span class="mdl-layout-title">Categories</span>
+            <nav class="mdl-navigation">
+               <a class="mdl-navigation__link" href="">Books</a>
+               <a class="mdl-navigation__link" href="">Clothing</a>
+               <a class="mdl-navigation__link" href="">Electronics</a>
+               <a class="mdl-navigation__link" href="">Footwear</a>
+               <a class="mdl-navigation__link" href="">Movies</a>
+               <a class="mdl-navigation__link" href="">Toys</a> 
+            </nav>
+         </div>
+         <main class="mdl-layout__content">
+            <div class="page-content">
+               <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+                  <header class="mdl-layout__header">
+                     <div class="mdl-layout__header-row">
+                        <span class="mdl-layout-title">E-Wagon</span>
+                        <div class="mdl-layout-spacer"></div>
+                        <nav class="mdl-navigation mdl-layout--large-screen-only">
+                           <a class="mdl-navigation__link" href=""> Sign-Up</a>
+                           <a class="mdl-navigation__link" href="">Login</a>
+                           <a class="mdl-navigation__link" href="">Cart</a>
+                           <a class="mdl-navigation__link" href="">Wishlist</a>
+                        </nav>
+                     </div>
+                  </header>
+                  <main class="mdl-layout__content">
+                     <div class="page-content">
+                        <!-- Conent Goes Here -->
+                     </div>
+                  </main>
+               </div>
+            </div>
+         </main>
+      </div>
+   </body>
+</html>
